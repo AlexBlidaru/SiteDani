0f2696c15a4001bdf949c35cff6ec5e4497fe0fd
AlexBlidaru <alexblidaru@yahoo.com>
2024-08-14 15:53:19 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+  <script>
+        function activateLink(sectionId) {
+            // Ascunde toate secțiunile
+            const sections = document.querySelectorAll('section');
+            sections.forEach(section => {
+                section.style.display = 'none';
+            });
+
+            // Afișează secțiunea corespunzătoare
+            const section = document.getElementById(sectionId);
+            section.style.display = 'block';
+        }
+    </script>
